3f918cbe27edc0fd1b5e1728653b0ea9dfdb0c6b
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-20 01:09:45 +0300

    Change Functions Parameter

--- DrawLine.cpp
+++ DrawLine.cpp
@@ -1,12 +1,12 @@
 #include <windows.h>
 
-void DrawLineDDA(HDC hdc , int x1, int y1, int x2, int y2){
+void DrawLineDDA(HDC hdc , int x1, int y1, int x2, int y2 , COLORREF c){
 
 }
-void DrawLineMidpoint(HDC hdc , int x1, int y1, int x2, int y2){
+void DrawLineMidpoint(HDC hdc , int x1, int y1, int x2, int y2 , COLORREF c){
 
 }
 
-void DrawLineParametric(HDC hdc , int x1, int y1, int x2, int y2){
+void DrawLineParametric(HDC hdc , int x1, int y1, int x2, int y2 , COLORREF c){
 
 }
