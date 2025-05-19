32ec1e916490a3fc732a5d2aac43fe6f4f7929cf
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-20 00:56:22 +0300

    Add Menu and Some Globale Variables for draw

--- RunApp.cpp
+++ RunApp.cpp
@@ -44,11 +44,131 @@ enum DrawShap {
     LOAD_SCREEN
 };
 
+struct Point {
+    int x , y ;
+};
+
+Point startPoint ;
+DrawShap currShape = NONE ;
+COLORREF currColor = RGB(0,0,0); // black
+bool isDrawing = false ;
+
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
+
+void creatDrawingMenu(HWND hwnd) {
+    HMENU hMenu = CreateMenu();
+    HMENU hLineMenu = CreatePopupMenu();
+    HMENU hCircleMenu = CreatePopupMenu();
+    HMENU hEllipseMenu = CreatePopupMenu();
+    HMENU hFillMenu = CreatePopupMenu();
+    HMENU hFloodMenu = CreatePopupMenu();
+    HMENU hClipMenu = CreatePopupMenu();
+    HMENU hColorMenu = CreatePopupMenu();
+
+    // For Line
+    AppendMenu(hLineMenu , MF_STRING , LINE_DDA , "DDA");
+    AppendMenu(hLineMenu , MF_STRING , LINE_MIDPOINT , "Midpoint");
+    AppendMenu(hLineMenu , MF_STRING , LINE_PARAMETRIC , "Parametric");
+    AppendMenu(hMenu , MF_POPUP , (UINT_PTR)hLineMenu , "Lines");
+    //-----------------------------------
+    // Circle
+    AppendMenu(hCircleMenu, MF_STRING, CIRCLE_DIRECT, "Direct");
+    AppendMenu(hCircleMenu, MF_STRING, CIRCLE_POLAR, "Polar");
+    AppendMenu(hCircleMenu, MF_STRING, CIRCLE_ITERATIVE_POLAR, "Iterative Polar");
+    AppendMenu(hCircleMenu, MF_STRING, CIRCLE_MIDPOINT, "Midpoint");
+    AppendMenu(hCircleMenu, MF_STRING, CIRCEL_MODIFIED_MIDPOINT, "Modified Midpoint");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hCircleMenu, "Circles");
+    //-----------------------------------
+    // Ellips
+    AppendMenu(hEllipseMenu, MF_STRING, ELLIPSE_DIRECT, "Direct");
+    AppendMenu(hEllipseMenu, MF_STRING, ELLIPSE_POLAR, "Polar");
+    AppendMenu(hEllipseMenu, MF_STRING, ELLIPSE_MIDPOINT, "Midpoint");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hEllipseMenu, "Ellipses");
+    //-----------------------------------
+    // Filling
+    AppendMenu(hFillMenu, MF_STRING, FILL_CIRCLE_WITH_LINE, "Circle Fill With Lines");
+    AppendMenu(hFillMenu, MF_STRING, FILL_CIRCLE_WITH_CIRCLE, "Circle Fill With Circles");
+    AppendMenu(hFillMenu, MF_STRING, FILL_HERMITE, "Square Fill With Hermite");
+    AppendMenu(hFillMenu, MF_STRING, FILL_BEZIER, "Rectangle Fill With Bezier");
+    AppendMenu(hFillMenu, MF_STRING, FILL_CONVEX, "Convex Filling");
+    AppendMenu(hFillMenu, MF_STRING, FILL_NONCONVEX, "Non-Convex Filling");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFillMenu, "Filling");
+    //-----------------------------------
+    // Flood Fill
+    AppendMenu(hFloodMenu, MF_STRING, FLOOD_FILL_RECURSIVE, "Flood Fill Recursive");
+    AppendMenu(hFloodMenu, MF_STRING, FLOOD_FILL_NONRECURSIVE, "Flood Fill Non-Recursive");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFillMenu, "Flood Fill");
+    //-----------------------------------
+    // Spline
+    AppendMenu(hMenu, MF_STRING, SPLINE_CARDINAL, "Cardinal Spline");
+    //-----------------------------------
+    // Clipping
+    AppendMenu(hClipMenu, MF_STRING, CLIP_POINT_RECT, "Point Clip (Rect)");
+    AppendMenu(hClipMenu, MF_STRING, CLIP_LINE_RECT, "Line Clip (Rect)");
+    AppendMenu(hClipMenu, MF_STRING, CLIP_POLYGON_RECT, "Polygon Clip (Rect)");
+    AppendMenu(hClipMenu, MF_STRING, CLIP_POINT_SQUARE, "Point Clip (Square)");
+    AppendMenu(hClipMenu, MF_STRING, CLIP_LINE_SQUARE, "Line Clip (Square)");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hClipMenu, "Clipping");
+    //-----------------------------------
+    // Color
+    AppendMenu(hColorMenu, MF_STRING, RED, "Red");
+    AppendMenu(hColorMenu, MF_STRING, GREEN, "Green");
+    AppendMenu(hColorMenu, MF_STRING, BLUE, "Blue");
+    AppendMenu(hColorMenu, MF_STRING, BLACK, "Black");
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hColorMenu, "Color");
+    //-----------------------------------
+
+    AppendMenu(hMenu, MF_STRING, CLEAR_SCREEN, "Clear Screen");
+    AppendMenu(hMenu, MF_STRING, SAVE_SCREEN, "Save");
+    AppendMenu(hMenu, MF_STRING, LOAD_SCREEN, "Load");
+
+    SetMenu(hwnd , hMenu);
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CREATE:
+            creatDrawingMenu(hwnd);
+            break;
+        case WM_COMMAND:
+            DrawShap cmd = (DrawShap)(LOWORD(wParam));
+            if (cmd >= RED && cmd <= BLACK) {
+                if (cmd == RED)
+                    currColor = RGB(255 , 0 , 0);
+                else if (cmd == GREEN)
+                    currColor = RGB(0 , 255 , 0);
+                else if (cmd == BLUE)
+                    currColor = RGB(0 , 0 , 255);
+                else
+                    currColor = RGB(0,0,0);
+            }
+            else if (cmd == CLEAR_SCREEN) {
+                InvalidateRect(hwnd , NULL , TRUE);
+            }
+            else if (cmd == SAVE_SCREEN) {
+                // cooooooooodddddddeeeee
+            }
+            else if (cmd == LOAD_SCREEN) {
+                // cooooooooodddddddeeeee
+            }
+            else
+                currShape = cmd ;
+            break;
+        case WM_LBUTTONDOWN:
+            startPoint.x = LOWORD(lParam);
+            startPoint.x = HIWORD(lParam);
+            isDrawing = true ;
+            break;
+        case WM_LBUTTONUP:
 
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
