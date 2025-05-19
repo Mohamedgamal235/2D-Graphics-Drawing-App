9941f2c67c68e08e581721dd0afe0a84d7cbaed2
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-19 22:44:23 +0300

    Initialize Functions

--- DrawFilling.cpp
+++ DrawFilling.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Gemmy on 19/05/2025.
-//
+#include <windows.h>
+
+void FillCircleWithLines(HDC hdc ,int xc, int yc, int r, int quarter){
+
+}
+void FillCircleWithCircles(HDC hdc ,int xc, int yc, int r, int quarter){
+
+}
+void FillSquareWithHermite(HDC hdc){
+
+}
+void FillRectangleWithBezier(HDC hdc){
+
+}
+void ConvexFilling(HDC hdc){
+
+}
+void NonConvexFilling(HDC hdc){
+
+}
