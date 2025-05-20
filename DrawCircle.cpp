2213fac363d93b45a0a4f26dc0267683f12ed246
Anas-Adel <anasadel898@gmail.com>
2025-05-20 15:58:09 +0300

    DrawCircleMidpoint

--- DrawCircle.cpp
+++ DrawCircle.cpp
@@ -23,7 +23,29 @@ void DrawCircleIterativePolar(HDC hdc, int xc, int yc, int r){
 
 // Anas Adel
 void DrawCircleMidpoint(HDC hdc, int xc, int yc, int r){
-
+    int x = 0;
+    int y = R;
+    int d = 1 - R;
+    while (x <= y) {
+        SetPixel(hdc, xc + x, yc + y, RGB(0, 0, 0));
+        SetPixel(hdc, xc - x, yc + y, RGB(0, 0, 0));
+        SetPixel(hdc, xc + x, yc - y, RGB(0, 0, 0));
+        SetPixel(hdc, xc - x, yc - y, RGB(0, 0, 0));
+        SetPixel(hdc, xc + y, yc + x, RGB(0, 0, 0));
+        SetPixel(hdc, xc - y, yc + x, RGB(0, 0, 0));
+        SetPixel(hdc, xc + y, yc - x, RGB(0, 0, 0));
+        SetPixel(hdc, xc - y, yc - x, RGB(0, 0, 0));
+    
+        if (d < 0) { 
+            x++;
+            d += 2 * x + 3;
+        }
+        else {
+            x++;
+            y--;
+            d += 2 * x - 2 * y + 5;
+        }
+    }
 }
 
 // -------------------------------------------
