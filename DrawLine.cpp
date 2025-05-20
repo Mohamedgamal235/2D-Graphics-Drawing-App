7125c9e951b5e2380e385f335885a97ea0758f7e
Anas-Adel <anasadel898@gmail.com>
2025-05-20 16:24:06 +0300

    DrawLineMidpoint

--- DrawLine.cpp
+++ DrawLine.cpp
@@ -8,8 +8,39 @@ void DrawLineDDA(HDC hdc , int x1, int y1, int x2, int y2 , COLORREF c){
 // -------------------------------------------
 
 // Anas
-void DrawLineMidpoint(HDC hdc , int x1, int y1, int x2, int y2 , COLORREF c){
+void DrawLineMidpoint(HDC hdc, int x1, int y1, int x2, int y2, COLORREF c) {
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x2 >= x1) ? 1 : -1;
+    int sy = (y2 >= y1) ? 1 : -1;
 
+    int x = x1;
+    int y = y1;
+
+    if (dx > dy) {
+        int d = 2 * dy - dx;
+        for (int i = 0; i <= dx; i++) {
+            SetPixel(hdc, x, y, c);
+            if (d >= 0) {
+                y += sy;
+                d -= 2 * dx;
+            }
+            x += sx;
+            d += 2 * dy;
+        }
+    }
+    else {
+        int d = 2 * dx - dy;
+        for (int i = 0; i <= dy; i++) {
+            SetPixel(hdc, x, y, c);
+            if (d >= 0) {
+                x += sx;
+                d -= 2 * dy;
+            }
+            y += sy;
+            d += 2 * dx;
+        }
+    }
 }
 
 // -------------------------------------------
