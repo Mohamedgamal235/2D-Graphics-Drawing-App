d2c5df6ff17575a7049fa6bd88c3288343dea04f
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-29 23:05:45 +0300

    Make File for point clipping

---
+++ DrawPointClipping.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+using namespace std;
+
+
+
+
+// Anas Adel
+void ClipPointRectangle(HDC hdc){
+
+}
+
+
+// -------------------------------------------
+
+
+// Ahmed Mohsen
+void ClipPointSquare(HDC hdc){
+
+
+}
