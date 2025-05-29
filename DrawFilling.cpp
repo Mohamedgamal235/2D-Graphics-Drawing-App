a9ce3957a492f4eda89e861e295c61a14f069b99
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-29 22:41:36 +0300

    Make convex Fill algorithm

--- DrawFilling.cpp
+++ DrawFilling.cpp
@@ -1,10 +1,31 @@
 #include <windows.h>
 #include <list>
-#include <algorithm>
 using namespace std;
+
+struct Point {
+    int x, y;
+    Point(int x = 0 , int y = 0) : x(x) , y(y) {}
+};
+
+void swap(int& x , int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void swap(Point& p1 , Point& p2) {
+    swap(p1.x , p2.x);
+    swap(p1.y , p2.y);
+}
+
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+
 // Ahmed Mohsen
 void FillCircleWithLines(HDC hdc ,int xc, int yc, int r, int quarter){
-
+    
 }
 
 // -------------------------------------------
@@ -29,24 +50,68 @@ void FillRectangleWithBezier(HDC hdc){
 }
 
 // -------------------------------------------
-
+// -------------------------------------------
+// -------------------------------------------
 // Mohamed Gamal
-void ConvexFilling(HDC hdc){
 
+struct Entry {
+    int xmin , xmax ;
+};
+
+void InitEntrites(Entry table[]) {
+    for (int i = 0 ; i < 800 ; i++) {
+        table[i].xmin = INT_MAX ;
+        table[i].xmax = INT_MIN ;
+    }
 }
 
-// -------------------------------------------
+void ScanEdge(Point p1 , Point p2 , Entry table[]) {
+    if (p1.y == p2.y) return ;
+    if (p1.y > p2.y) swap(p1,p2);
+    double mInverse = (double) (p2.x - p1.x) / (p2.y - p1.y) ; 
+    double x = p1.x ;
+    int y = p1.y ; 
+    while (y < p2.y) {
+        if (x < table[y].xmin) table[y].xmin = x ;
+        if (x > table[y].xmax) table[y].xmax = x ;
+        x += mInverse ;
+        y++ ;
+    }
+}
+
+void DrawScanLine(HDC hdc , Entry table[] , COLORREF color) {
+    for (int i = 0 ; i < 800 ; i++){
+        if (table[i].xmin <= table[i].xmax) {
+            for (int x = table[i].xmin ; x <= table[i].xmax ; x++) {
+                SetPixel(hdc , x , i , color);
+            }
+        }
+    }
+}
+
+void ConvexFilling(HDC hdc , Point p[] , int n , COLORREF color){
+    Entry *table = new Entry[800];
+    InitEntrites(table);
+    Point p1 = p[n-1];
+    for (int i = 0 ; i < n ; i++) {
+        Point p2 = p[i];
+        ScanEdge(p1 , p2 , table);
+        p1 = p2;
+    }
+    DrawScanLine(hdc , table , color);
+    delete table ; 
+}
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
 
 // Osama
 struct PolygonPoint{
     int x, y;
     PolygonPoint(int x = 0, int y = 0):x(x),y(y){};
 };
-void swap(int &x , int &y){
-    int temp = x;
-    x = y;
-    y = temp;
-}
+
 void swap(PolygonPoint& p1, PolygonPoint& p2){
     swap(p1.x,p2.x);
     swap(p1.y,p2.y);
@@ -59,14 +124,20 @@ struct EdgeInfo{
     }
     EdgeInfo(double x = 0 ,double m_inverse = 0, int y = 0):x(x),m_inverse(m_inverse),y_max(y){};
 };
-typedef list<EdgeInfo> EdgeList;
+
+
+typedef std::list<EdgeInfo> EdgeList;
 typedef EdgeList EdgeTable[800];
+
+
 void edgeToTable(PolygonPoint& p1,PolygonPoint& p2,EdgeTable tbl){
     if(p1.y == p2.y) return;
     if(p1.y > p2.y) swap(p1,p2);
     double m_inverse = (double) (p2.x - p1.x) / (p2.y - p1.y);
     tbl[p1.y].push_back(EdgeInfo(p1.x,m_inverse,p2.y));
 };
+
+
 void polygonToTable(PolygonPoint points[] ,int n,EdgeTable tbl){
     PolygonPoint p1 = points[n-1];
     for (int i = 0; i < n; ++i) {
@@ -75,6 +146,8 @@ void polygonToTable(PolygonPoint points[] ,int n,EdgeTable tbl){
         p1 = points[i];
     }
 }
+
+
 void NonConvexFilling(HDC hdc,PolygonPoint points[],int n,COLORREF color){
     // initialize
     EdgeTable table;
