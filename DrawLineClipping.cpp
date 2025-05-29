d2b81536cca1b5ff85ffbc133fcf76b04511499f
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-29 23:16:34 +0300

    Make small updates

--- DrawLineClipping.cpp
+++ DrawLineClipping.cpp
@@ -34,33 +34,44 @@ void ClipLineRectangle(HDC hdc, int xs, int ys, int xe, int ye,int x_left,int x_
     DrawLineDDA(hdc,x_left,y_top,x_left,y_bottom,RGB(0,0,0));
     DrawLineDDA(hdc,x_left,y_bottom,x_right,y_bottom,RGB(0,0,0));
     DrawLineDDA(hdc,x_right,y_top,x_right,y_bottom,RGB(0,0,0));
+
     double x1 = xs, y1 = ys, x2 = xe, y2 = ye;
     OutCode out1 = getOutCode(x1,y1,x_left,x_right,y_top,y_bottom);
     OutCode out2 = getOutCode(x2,y2,x_left,x_right,y_top,y_bottom);
+
     while( (out1.all || out2.all) && ! (out1.all & out2.all)){
         double xi,yi;
         if(out1.all){
-            if (out1.left) vertical_intersection(x1,y1,x2,y2,x_left,xi,yi);
-            else if(out1.top) horizontal_intersection(x1,y1,x2,y2,y_top,xi,yi);
-            else if(out1.right) vertical_intersection(x1,y1,x2,y2,x_right,xi,yi);
-            else horizontal_intersection(x1,y1,x2,y2,y_bottom,xi,yi);
+            if (out1.left) 
+                vertical_intersection(x1,y1,x2,y2,x_left,xi,yi);
+            else if(out1.top) 
+                horizontal_intersection(x1,y1,x2,y2,y_top,xi,yi);
+            else if(out1.right) 
+                vertical_intersection(x1,y1,x2,y2,x_right,xi,yi);
+            else 
+                horizontal_intersection(x1,y1,x2,y2,y_bottom,xi,yi);
             x1 = xi;
             y1 = yi;
             out1 = getOutCode(x1,y1,x_left,x_right,y_top,y_bottom);
         }
         else {
-            if (out2.left) vertical_intersection(x1,y1,x2,y2,x_left,xi,yi);
-            else if(out2.top) horizontal_intersection(x1,y1,x2,y2,y_top,xi,yi);
-            else if(out2.right) vertical_intersection(x1,y1,x2,y2,x_right,xi,yi);
-            else horizontal_intersection(x1,y1,x2,y2,y_bottom,xi,yi);
+            if (out2.left) 
+                vertical_intersection(x1,y1,x2,y2,x_left,xi,yi);
+            else if(out2.top) 
+                horizontal_intersection(x1,y1,x2,y2,y_top,xi,yi);
+            else if(out2.right) 
+                vertical_intersection(x1,y1,x2,y2,x_right,xi,yi);
+            else 
+                horizontal_intersection(x1,y1,x2,y2,y_bottom,xi,yi);
             x2 = xi;
             y2 = yi;
             out2 = getOutCode(x2,y2,x_left,x_right,y_top,y_bottom);
         }
     }
-    if (!out1.all && !out2.all){
+    
+    if (!out1.all && !out2.all)
         DrawLineDDA(hdc,(int)x1,(int)y1,(int)x2,(int)y2,RGB(255,0,0));
-    }
+    
 }
 
 // -------------------------------------------
