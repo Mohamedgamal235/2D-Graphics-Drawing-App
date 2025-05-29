de27008e07e8939e0812a02b9ac951be0241e2bb
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-05-30 00:20:08 +0300

    Draw Square and Rectangle

--- DrawWindow.cpp
+++ DrawWindow.cpp
@@ -4,9 +4,16 @@
 void DrawRectungle(HDC hdc , int left , int top , int right , int bottom){
     HPEN pen = CreatePen(PS_SOLID , 3 , RGB(0 , 0 , 0));
     HPEN oldPen = (HPEN)SelectObject(hdc , pen);
-
     Rectangle(hdc , left , top , right , bottom);
-
     SelectObject(hdc , oldPen);
+    DeleteObject(pen);
+}
+
+
+void DrawSquare(HDC hdc, int left, int top, int sideLen) {
+    HPEN pen = CreatePen(PS_SOLID, 3, RGB(0, 0, 0));
+    HPEN oldPen = (HPEN)SelectObject(hdc, pen);
+    Rectangle(hdc, left, top, left + sideLen, top + sideLen);
+    SelectObject(hdc, oldPen);
     DeleteObject(pen);
 }
