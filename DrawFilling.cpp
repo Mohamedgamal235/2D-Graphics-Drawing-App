a9288d2efa995653dc363487b64bf59102ce86ae
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-06-01 21:06:45 +0300

    Make Filling Circle with line implementation

--- DrawFilling.cpp
+++ DrawFilling.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <list>
 #include <cmath>
+#include "DrawLineClipping.cpp"
 using namespace std;
 
 struct Point {
@@ -23,10 +24,46 @@ void swap(Point& p1 , Point& p2) {
 // -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
-
-// Ahmed Mohsen
-void FillCircleWithLines(HDC hdc ,int xc, int yc, int r, int quarter){
+// Ahmed Mohsenvoid
+void FillCircleWithLines(HDC hdc, int xc, int yc, int r, int quarter, COLORREF color) {
+    // Quarter 1: Top Right
+    // Quarter 2: Top Left
+    // Quarter 3: Bottom Left
+    // Quarter 4: Bottom Right
     
+    switch(quarter) {
+        case 1: // Top Right
+            for(int x = xc; x <= xc + r; x++) {
+                int y1 = yc;
+                int y2 = yc - (int)sqrt(r * r - (x - xc) * (x - xc));
+                DrawLineMidpoint(hdc , x , y1 , x , y2 , color);
+            }
+            break;
+            
+        case 2: // Top Left
+            for(int x = xc; x >= xc - r; x--) {
+                int y1 = yc;
+                int y2 = yc - (int)sqrt(r * r - (x - xc) * (x - xc));
+                DrawLineMidpoint(hdc , x , y1 , x , y2 , color);
+            }
+            break;
+            
+        case 3: // Bottom Left
+            for(int x = xc; x >= xc - r; x--) {
+                int y1 = yc;
+                int y2 = yc + (int)sqrt(r * r - (x - xc) * (x - xc));
+                DrawLineMidpoint(hdc , x , y1 , x , y2 , color);
+            }
+            break;
+            
+        case 4: // Bottom Right
+            for(int x = xc; x <= xc + r; x++) {
+                int y1 = yc;
+                int y2 = yc + (int)sqrt(r * r - (x - xc) * (x - xc));
+                DrawLineMidpoint(hdc , x , y1 , x , y2 , color);
+            }
+            break;
+    }
 }
 
 // -------------------------------------------
